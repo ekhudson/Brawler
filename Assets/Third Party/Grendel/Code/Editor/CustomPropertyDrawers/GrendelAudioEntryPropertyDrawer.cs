cbaf7d82f20da452e19ee9d16c8e01d0428631c1
Elliot Hudson <ekhudson@gmail.com>
2014-02-05 23:26:21 -0800

    +Work started on GrendelAudioEntry custom property drawer

---
+++ Assets/Third Party/Grendel/Code/Editor/CustomPropertyDrawers/GrendelAudioEntryPropertyDrawer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomPropertyDrawer(typeof(GrendelAudioEntry))]
+public class GrendelAudioEntryPropertyDrawer : PropertyDrawer 
+{
+	public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) 
+	{
+		GUI.Label(position, label);
+	}
+}
