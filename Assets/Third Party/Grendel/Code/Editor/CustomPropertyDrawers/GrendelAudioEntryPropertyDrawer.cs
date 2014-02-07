fbacd834ad377456bb131bea28791bede993b768
Elliot Hudson <ekhudson@gmail.com>
2014-02-06 22:36:57 -0800

    +AudioEntry Property Drawer

--- Assets/Third Party/Grendel/Code/Editor/CustomPropertyDrawers/GrendelAudioEntryPropertyDrawer.cs
+++ Assets/Third Party/Grendel/Code/Editor/CustomPropertyDrawers/GrendelAudioEntryPropertyDrawer.cs
@@ -7,6 +7,16 @@ public class GrendelAudioEntryPropertyDrawer : PropertyDrawer
 {
 	public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) 
 	{
-		GUI.Label(position, label);
+		//GUILayout.BeginHorizontal();
+
+		label = EditorGUI.BeginProperty(position, label, property);
+		property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, label, true);
+		EditorGUI.EndProperty();
+
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.PropertyField(property.FindPropertyRelative("AudioBankNumber"));
+		EditorGUILayout.PropertyField(property.FindPropertyRelative("AudioClipNumber"));
+		EditorGUILayout.EndHorizontal();
+		//GUILayout.EndHorizontal();
 	}
 }
